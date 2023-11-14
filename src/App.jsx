7d74b7f7a7fa478f1c225835a5898fbd34350033
joaquin-espinosa733 <juaco.enero9@gmail.com>
2023-11-14 01:25:55 -0300

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.scss';
+import Home from './components/home/Home';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className='count container-xxl'>
+        <Home />
+        <h1 >hola mundo</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <div>
+        <h1 className="container-xxl">Home</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
